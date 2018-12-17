f34ff4b6d9a3e09c67945fbda2d39f03f46f0d03
Patricia Putriningsih Pesahnia Djami <patricia.djami@macio.de>
2018-12-17 14:20:03 +0100

    implemented everything aside from get_fingers_count()

--- detector/hand_detector.py
+++ detector/hand_detector.py
@@ -1,11 +1,12 @@
-import cv2
+import cv2 as cv
+import numpy as np
 from math import acos, pi
 from config import SKIN_LOWER_BOUND, SKIN_UPPER_BOUND
 
 
 class HandDetector:
     def __init__(self):
-        self.fgbg = cv2.createBackgroundSubtractorMOG2()
+        self.fgbg = cv.createBackgroundSubtractorMOG2()
 
     def get_hand_position(self, frame):
         position = None
@@ -22,22 +23,35 @@ class HandDetector:
 
     def _get_interesting_pixels_mask(self, frame):
         # http://docs.opencv.org/3.0-beta/doc/py_tutorials/py_imgproc/py_filtering/py_filtering.html#filtering
-        # http://docs.opencv.org/3.1.0/db/d5c/tutorial_py_bg_subtraction.html
+        # (blurring)
+        blur = cv.GaussianBlur(frame, (5, 5), 0)
         # http://docs.opencv.org/3.0-beta/doc/py_tutorials/py_imgproc/py_colorspaces/py_colorspaces.html#converting-colorspaces
+        hsv = cv.cvtColor(blur, cv.COLOR_BGR2HSV)
+        mask = cv.inRange(hsv, SKIN_LOWER_BOUND, SKIN_UPPER_BOUND)
+        mask = cv.erode(mask, None, iterations=2)
+        mask = cv.dilate(mask, None, iterations=2)
+        res = cv.bitwise_and(frame, frame, mask=mask)
+        # http://docs.opencv.org/3.1.0/db/d5c/tutorial_py_bg_subtraction.html
+        # (subtract background)
+        fgmask = self.fgbg.apply(res)
         # http://docs.opencv.org/3.0-beta/doc/py_tutorials/py_imgproc/py_thresholding/py_thresholding.html#thresholding
-
-        return None
+        # ret, thresh = cv.threshold(fgmask, 100, 255, cv.THRESH_BINARY)
+        return fgmask
 
     @staticmethod
     def _get_transformed_pixels_mask(mask):
         # http://opencv-python-tutroals.readthedocs.io/en/latest/py_tutorials/py_imgproc/py_morphological_ops/py_morphological_ops.html
+        kern = np.ones((5,5), 'uint8')
+        morph = cv.morphologyEx(mask,cv.MORPH_CLOSE, kern)
         # http://docs.opencv.org/3.0-beta/doc/py_tutorials/py_imgproc/py_filtering/py_filtering.html#filtering
+        bil = cv.bilateralFilter(morph, 5, 160, 160)
 
-        return None
+        return bil
 
     @staticmethod
     def _get_fingers_count(hand_contour):
         # https://docs.opencv.org/3.1.0/dd/d49/tutorial_py_contour_features.html
+
         # https://docs.opencv.org/3.0-beta/doc/py_tutorials/py_imgproc/py_contours/py_contours_more_functions/py_contours_more_functions.html
         # https://en.wikipedia.org/wiki/Law_of_cosines
 
